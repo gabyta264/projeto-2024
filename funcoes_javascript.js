a1ffb2bc30b2b434dda158573899011edba8efa3
gabyta264 <105683132+gabyta264@users.noreply.github.com>
2024-04-17 13:54:34 -0300

    Create funcoes_javascript.js

---
+++ funcoes_javascript.js
@@ -0,0 +1,15 @@
+  function somar(num1, num2) {
+    return nun1+num2
+  }
+
+  function diminuir(num1, num2) {
+    return nun1-num2
+  } 
+
+ function dividir (num1, num2) {
+    return nun1/num2
+  } 
+
+  function multiplicar (num1, num2) {
+    return nun1*num2
+  } 
